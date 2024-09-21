d58acc3cac2b300b12017c2ab3c46e9982e892b4
cem.topkaya <cem.topkaya@ulakhaberlesme.com.tr>
2024-09-21 16:43:00 +0000

    status, rol ve tracker ekleniyor

--- .devcontainer/setup_ruby_env.sh
+++ .devcontainer/setup_ruby_env.sh
@@ -94,6 +94,188 @@ aktivateRestAPI() {
 
     mysql -h $MYSQL_HOST -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${DATABASE}" -e "UPDATE settings SET value='${REST_API_VALUE}', updated_on='${CURRENT_TIME}' WHERE name='${REST_API_NAME}';"
     mysql -h $MYSQL_HOST -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${DATABASE}" -e "UPDATE settings SET value='${JSONP_VALUE}', updated_on='${CURRENT_TIME}' WHERE name='${JSONP_NAME}';"
+
+    # Tracker'lar yoksa ekleniyor
+    # sql="INSERT IGNORE INTO ${DATABASE}.trackers 
+    #             (name      , description, position, is_in_roadmap, fields_bits, default_status_id) 
+    #      VALUES ('Epic'    ,     '',        11,             1,           8,          1),
+    #             ('Story'   ,     '',        12,             1,           0,          1),
+    #             ('Task'    ,     '',        8 ,             1,           0,          1),
+    #             ('Sub-task',     '',        9 ,             1,           0,          1),
+    #             ('Bug'     ,     '',        10,             1,           0,          1),
+    #             ('Feature' ,     '',        13,             1,           0,          1);"
+
+    # mysql -h $MYSQL_HOST -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${DATABASE}" -e "${sql}"
+
+    # Sadece belirtilen tracker isimleri yoksa ekliyor
+    sql="
+    INSERT INTO ${DATABASE}.trackers 
+        (name, description, position, is_in_roadmap, fields_bits, default_status_id)
+    SELECT * FROM (
+        SELECT 'Epic' AS name, '' AS description, 11 AS position, 1 AS is_in_roadmap, 8 AS fields_bits, 1 AS default_status_id
+        UNION ALL SELECT 'Story', '', 12, 1, 0, 1
+        UNION ALL SELECT 'Task', '', 8, 1, 0, 1
+        UNION ALL SELECT 'Sub-task', '', 9, 1, 0, 1
+        UNION ALL SELECT 'Bug', '', 10, 1, 0, 1
+        UNION ALL SELECT 'Feature', '', 13, 1, 0, 1
+    ) AS new_trackers
+    WHERE new_trackers.name NOT IN (SELECT name FROM ${DATABASE}.trackers);
+    "
+    mysql -h $MYSQL_HOST -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${DATABASE}" -e "${sql}"
+
+    # Sadece belirtilen issue durumları yoksa ekliyor
+    sql="
+    INSERT INTO ${DATABASE}.issue_statuses 
+        (name, is_closed, position, default_done_ratio)
+    SELECT * FROM (
+        SELECT 'Open' AS name, 0 AS is_closed, 2 AS position, NULL AS default_done_ratio
+        UNION ALL SELECT 'Closed', 1, 14, NULL
+        UNION ALL SELECT 'Done', 1, 12, NULL
+        UNION ALL SELECT 'In Review', 0, 6, NULL
+        UNION ALL SELECT 'In Progress', 0, 5, NULL
+        UNION ALL SELECT 'Pending', 0, 9, NULL
+        UNION ALL SELECT 'Reopened', 0, 13, NULL
+        UNION ALL SELECT 'Rejected', 0, 3, NULL
+        UNION ALL SELECT 'Deferred', 0, 4, NULL
+        UNION ALL SELECT 'Resolved', 0, 10, NULL
+        UNION ALL SELECT 'Blocked', 0, 8, NULL
+        UNION ALL SELECT 'In Test', 0, 11, NULL
+        UNION ALL SELECT 'Needs Identified', 0, 7, NULL
+        UNION ALL SELECT 'TO DO', 0, 1, NULL
+    ) AS new_statuses
+    WHERE new_statuses.name NOT IN (SELECT name FROM ${DATABASE}.issue_statuses);
+    "
+
+    mysql -h $MYSQL_HOST -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${DATABASE}" -e "${sql}"
+
+    # Redmine rollerinin optimize edilmiş eklenmesi
+    sql="INSERT INTO redmine.roles (name,`position`,assignable,builtin,permissions,issues_visibility,users_visibility,time_entries_visibility,all_roles_managed,settings) VALUES
+	 ('Non member',0,1,1,'---
+- :view_issues
+- :view_news
+- :view_messages
+','default','all','all',1,NULL),
+	 ('Anonymous',0,1,2,'---
+- :view_issues
+- :view_news
+- :view_messages
+','default','all','all',1,NULL),
+	 ('Developer',1,1,0,'---
+- :view_messages
+- :view_issues
+- :add_issues
+- :edit_issues
+- :edit_own_issues
+- :copy_issues
+- :manage_issue_relations
+- :manage_subtasks
+- :add_issue_notes
+- :edit_issue_notes
+- :edit_own_issue_notes
+- :mention_users
+- :add_issue_watchers
+- :view_news
+- :view_time_entries
+- :log_time
+- :edit_own_time_entries
+- :view_wiki_pages
+- :edit_wiki_pages
+- :rename_wiki_pages
+','default','all','all',1,'--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+permissions_all_trackers: !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+  view_issues: ''1''
+  add_issues: ''1''
+  edit_issues: ''1''
+  add_issue_notes: ''1''
+  delete_issues: ''1''
+permissions_tracker_ids: !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+  view_issues: []
+  add_issues: []
+  edit_issues: []
+  add_issue_notes: []
+  delete_issues: []
+'),
+	 ('Manager',2,1,0,'---
+- :add_project
+- :edit_project
+- :manage_members
+- :manage_versions
+- :add_subprojects
+- :save_queries
+- :view_messages
+- :view_documents
+- :add_documents
+- :edit_documents
+- :delete_documents
+- :view_files
+- :manage_files
+- :edit_issue_templates
+- :show_issue_templates
+- :manage_issue_templates
+- :view_issues
+- :add_issues
+- :edit_issues
+- :edit_own_issues
+- :copy_issues
+- :manage_issue_relations
+- :manage_subtasks
+- :set_issues_private
+- :set_own_issues_private
+- :add_issue_notes
+- :edit_issue_notes
+- :edit_own_issue_notes
+- :view_private_notes
+- :set_notes_private
+- :delete_issues
+- :mention_users
+- :view_issue_watchers
+- :add_issue_watchers
+- :delete_issue_watchers
+- :import_issues
+- :manage_categories
+- :view_news
+- :manage_news
+- :comment_news
+- :view_changesets
+- :browse_repository
+- :commit_access
+- :manage_related_issues
+- :manage_repository
+- :view_time_entries
+- :log_time
+- :edit_time_entries
+- :edit_own_time_entries
+- :manage_project_activities
+- :log_time_for_other_users
+- :import_time_entries
+- :view_wiki_pages
+- :view_wiki_edits
+- :export_wiki_pages
+- :edit_wiki_pages
+- :rename_wiki_pages
+- :delete_wiki_pages
+- :delete_wiki_pages_attachments
+- :view_wiki_page_watchers
+- :add_wiki_page_watchers
+- :delete_wiki_page_watchers
+- :protect_wiki_pages
+- :manage_wiki
+','default','all','all',1,'--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+permissions_all_trackers: !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+  view_issues: ''1''
+  add_issues: ''1''
+  edit_issues: ''1''
+  add_issue_notes: ''1''
+  delete_issues: ''1''
+permissions_tracker_ids: !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+  view_issues: []
+  add_issues: []
+  edit_issues: []
+  add_issue_notes: []
+  delete_issues: []
+');"
+
+    mysql -h $MYSQL_HOST -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${DATABASE}" -e "${sql}"
 }
 
 echo -e "\n\n----------------------------- ACTIVATE REST API WITH JSONP --------------------------------------------"
@@ -162,6 +344,9 @@ create_symlink_of_plugin(){
     # Farklı isimlerde eklentiler için docker-compose.yml içinde mount edilmi "/workspace/volume/redmine/redmine-plugins" dizini içinde yaratarak kodlayabilirsiniz
     ln -s -v /workspace/ornek_eklenti /usr/src/redmine/plugins/ornek_eklenti  && echo "link created" || echo "link creation failed"
 }
+
+echo -e "\n\n----------------------------- CREATE SYMBOLIC LINK FOR REDMINE PLUGIN ---------------------------------"
+create_symlink_of_plugin;
 
 echo -e "\n\n----------------------------- CREATE SYMBOLIC LINK FOR REDMINE PLUGIN ---------------------------------"
 create_symlink_of_plugin;
